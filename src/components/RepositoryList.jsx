057062c1296feaebdbc57a0f28e39c85e2e6043d
Mateus Alencar <mateuus.png@gmail.com>
2022-09-06 13:00:58 -0300

    chore: add RepositoryList

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,25 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Repository List</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="#">Access Repository</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="#">Access Repository</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="#">Access Repository</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
