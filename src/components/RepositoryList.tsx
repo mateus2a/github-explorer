06da777a6533c4dddf644de2a5c6ad968e2c403d
Mateus Alencar <mateuus.png@gmail.com>
2022-09-06 18:38:50 -0300

    refactor: to typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,10 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import '../styles/repositories.scss';
 import { RepositoryItem } from './RepositoryItem';
 
-import '../styles/repositories.scss';
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/users/mateus2a/repos')
