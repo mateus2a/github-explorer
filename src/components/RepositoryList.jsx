b317f558ca2b968018a0fb915835116e6fa89a13
Mateus Alencar <mateuus.png@gmail.com>
2022-09-06 14:21:04 -0300

    chore: fetch api github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from './RepositoryItem';
 
 import '../styles/repositories.scss';
+// https://api.github.com/users/mateus2a/repos
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/mateus2a/repos')
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, [repositories]);
+
   return (
     <section className="repository-list">
       <h1>Repository List</h1>
